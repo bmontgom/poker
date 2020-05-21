66c799dac4f91250c9d773ac6533a7493a412d0f
Barrett Montgomery <bingbong@bingbong.cong>
2020-05-20 23:57:10 -0500

    server working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 let app = require('express')();
+let express = require('express');
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
+let path = require('path');
+
+app.use(express.static(path.resolve(__dirname + '/../dist/poker/')));
 
 app.get('/', (req, res) => {
     console.log('got request');
-    res.send('<h1>Yo</h1>');
+    res.sendFile(path.resolve(__dirname + '/../dist/poker/index.html'));
 });
 
 http.listen(3000, () => {
