b67ffe4dfdcee5534df1e8a8583bd5aae3d33607
Barrett Montgomery <bingbong@bingbong.cong>
2020-05-25 23:11:53 -0500

    switch to ec2 socket url

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { InputTextModule } from 'primeng/inputtext';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 const config: SocketIoConfig = {
-    url: 'http://192.168.1.67:3000',
+    url: 'http://ec2-54-166-39-120.compute-1.amazonaws.com:3000',
     options: {
         rememberTransport: false,
         transports: ['websocket']
